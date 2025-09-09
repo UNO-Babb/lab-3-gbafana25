d9ccfba9de0419f39058d3005490dd402e8a389a
Gareth M. <gkm25@protonmail.com>
2025-09-09 13:06:17 -0500

    lab2 done

--- RPS.py
+++ RPS.py
@@ -1,25 +1,58 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gareth Moodley
+#Date: 9-9-2025
+#Assignment: Rock Paper Scissors
 import random
 
 def main():
   wins = 0
   ties = 0
   losses = 0
+  choices = ['R', 'P', 'S']
+  computer_choices = [("R", "Rock"), ("P", "Paper"), ("S", "Scissors")]
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
+  while True:
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    cpu_choice = computer_choices[random.randint(0, len(computer_choices)-1)] 
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
-
-  #In the end, print the stats
-  print("Wins \t Ties \t Losses")
-  print("---- \t ---- \t ------")
-  print(wins, "\t", ties , "\t", losses)
+    user_choice = input("Select Rock, Paper, or Scissors (R, P, S): ").upper()
+    if user_choice not in choices:
+        print("Error, please enter R, P, or S")
+    else:
+        #Determine winner and state what happened to the user
+        if cpu_choice[0] == "R" and user_choice == "P":
+            print("You win")
+            wins += 1
+        elif cpu_choice[0] == "R" and user_choice == "S":
+            print("You lose")
+            losses += 1
+        elif cpu_choice[0] == "P" and user_choice == "R":
+            print("You lose")
+            losses += 1
+        elif cpu_choice[0] == "P" and user_choice == "S":
+            print("You win")
+            wins += 1
+        elif cpu_choice[0] == "S" and user_choice == "P":
+            print("You lose")
+            losses += 1
+        elif cpu_choice[0] == "S" and user_choice == "R":
+            print("You win")
+            wins += 1
+        elif cpu_choice[0] == user_choice:
+            print("Tie")
+            ties += 1
+            
+        #Ask the user if they would like to play again.
+        game_continue = input("Would you like to play again (Y/N): ").upper()
+        if game_continue == "N":
+            #In the end, print the stats
+            print("Wins \t Ties \t Losses")
+            print("---- \t ---- \t ------")
+            print(wins, "\t", ties , "\t", losses)
+            break
+        elif game_continue == "Y":
+            continue
 
 if __name__ == '__main__':
   main()
--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,21 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gareth Moodley
+#Date: 9-9-2025
+#Assignment: Convert Fahrenheit to Celsius
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  
+  tempF = int(input("Input a temperature in Fahrenheit: "))
+
   #Convert that temperature to celsius, rounding to 1 decimal percision
+
+  tempC = ((tempF - 32) * 5) / 9
+  tempC = round(tempC, 1)
   #Output converted temperature.
-  tempF = 80
 
-  tempC = tempF / 2
+  print(tempF, "is", tempC, "degrees celsius.")
 
-  print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
